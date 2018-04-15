f2c020f26f0c4aba524d5d763ea505a8eb92e16b
Hector Cruz <Hector Cruz@HECTOR>
2018-04-15 20:44:43 -0300

    Agregado dos paquetes y dos nuevas clases.

---
+++ src/punto4/principal/Principal.java
@@ -0,0 +1,13 @@
+package punto4.principal;
+
+/**
+ *
+ * @author Hector Cruz
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
